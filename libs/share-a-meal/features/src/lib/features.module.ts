585a784373d997b12694079b82a3a692d53d5774
J.Seremak <janko2002@outlook.com>
2023-11-07 11:50:29 +0100

    Finished 6 maybe

--- libs/share-a-meal/features/src/lib/features.module.ts
+++ libs/share-a-meal/features/src/lib/features.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MealListComponent } from './meal/meal-list/meal-list.component';
+import { MealDetailComponent } from './meal/meal-detail/meal-detail.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [MealListComponent, MealDetailComponent],
 })
 export class FeaturesModule {}
