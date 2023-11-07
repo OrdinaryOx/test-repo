ffc39ced47f78ee98c58ff531351e50b3346f5a8
J.Seremak <janko2002@outlook.com>
2023-11-07 11:17:44 +0100

    Stap4

--- libs/backend/dto/src/index.ts
+++ libs/backend/dto/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/dto.module';
+export * from './lib/meal.dto';
--- libs/backend/features/src/index.ts
+++ libs/backend/features/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/meal.module';
+export * from './lib/meal/meal.module';
+export * from './lib/meal/backend-features-meal.module';
